f067374d0282edfce17274e26fc9b1bf37c556ac
sungsoo <142865231+sslee1210@users.noreply.github.com>
2026-07-14 21:28:56 +0900

    Use hash-router safe skip link on home

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -8,6 +8,7 @@ import {
 import { ServiceShowcase } from "@/components/site/ServiceShowcase";
 import { SiteFooter } from "@/components/site/SiteFooter";
 import { SiteHeader } from "@/components/site/SiteHeader";
+import { SkipLink } from "@/components/site/SkipLink";
 import { useRevealOnScroll } from "@/hooks/useRevealOnScroll";
 import "@/styles/pages/home.css";
 
@@ -16,9 +17,7 @@ export default function Home() {
 
   return (
     <div className="home-shell">
-      <a className="site-skip-link" href="#main-content">
-        본문으로 바로가기
-      </a>
+      <SkipLink />
       <SiteHeader transparentOnTop />
 
       <main id="main-content" className="home-main" tabIndex={-1}>
