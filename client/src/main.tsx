6539c34890842971fcd9658de8d887e668721991
sungsoo <142865231+sslee1210@users.noreply.github.com>
2026-07-31 11:09:06 +0900

    Load final experience arc override after visual refinements

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,5 +4,6 @@ import "./index.css";
 import "./styles/common/pfk-typography-standard.css";
 import "./styles/common/content-density-standard.css";
 import "./styles/pages/home-visual-refinement.css";
+import "./styles/pages/home-experience-arc-final.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
